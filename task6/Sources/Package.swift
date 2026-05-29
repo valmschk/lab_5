7d80725a5efdbbf6a27fb9502361939208a05745
valmschk <meshchenko05@mail.ru>
2026-05-29 09:49:09 +0300

    Update Package.swift

--- task6/Sources/Package.swift
+++ task6/Sources/Package.swift
@@ -1,1 +1,12 @@
+// swift-tools-version: 5.9
+import PackageDescription
 
+let package = Package(
+    name: "task6",
+    targets: [
+        .executableTarget(
+            name: "task6",
+            path: "Sources"
+        )
+    ]
+)
